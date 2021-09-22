f88e01ac68828a4d7a53303cc1b5371a748ddced
stolentino <stotino2000@yahoo.com>
2021-09-22 14:47:51 -0700

    test out new getters and setters for instructions, tags, yield an source

--- cookbook.php
+++ cookbook.php
@@ -1,23 +1,38 @@
 <?php
 include "classes/recipes.php";
-//testing new origin
+//testing new or
 
 $recipe1 = new Recipe();
 //echo $recipe1->source;
-$recipe1->source = "Terry Prieto";
+$recipe1->setSource("Terry Prieto");
 //echo $recipe1->source;
 $recipe1->setTitle("my frst recipe");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 $recipe2 = new Recipe();
-$recipe2->source = "Betty Crocker";
+$recipe2->setSource("Betty Crocker");
 $recipe2->setTitle("my second recipe");
 
 echo $recipe1->getTitle();
 foreach($recipe1->getIngredients() as $ing){
     echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
 }
+
+$recipe1->addInstruction("This is my first instruction");
+$recipe1->addInstruction("This is my second instruction");
+
+echo implode("\n", $recipe1->getInstructions());
+
+$recipe1->addTag("Breakfast");
+$recipe1->addTag("Main Course");
+
+echo implode(", ", $recipe1->getTags());
+
+$recipe1->setYield("6 servings");
+echo $recipe1->getYield();
+echo $recipe1->getSource();
+
 
 //echo $recipe1->displayRecipe();
 //echo $recipe2->displayRecipe();
