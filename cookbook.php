9ab05914454b123719f35336a2c45443e15e0232
stolentino <stotino2000@yahoo.com>
2021-09-29 16:46:02 -0700

    use recipes file

--- cookbook.php
+++ cookbook.php
@@ -1,33 +1,34 @@
 <?php
 include "classes/recipes.php";
 include "classes/render.php";
+include "inc/recipes.php";
 
-$recipe1 = new Recipe("my first recipe");
+//$recipe1 = new Recipe("my first recipe");
 //echo $recipe1->source;
-$recipe1->setSource("Terry Prieto");
+//$recipe1->setSource("Terry Prieto");
 //echo $recipe1->source;
 //$recipe1->setTitle("my frst recipe");
-$recipe1->addIngredient("egg", 1);
-$recipe1->addIngredient("flour", 2, "cup");
+//$recipe1->addIngredient("egg", 1);
+//$recipe1->addIngredient("flour", 2, "cup");
 
-$recipe2 = new Recipe("my second recipe");
-$recipe2->setSource("Betty Crocker");
+//$recipe2 = new Recipe("my second recipe");
+//$recipe2->setSource("Betty Crocker");
 //$recipe2->setTitle("my second recipe");
 
-$recipe1->addInstruction("This is my first instruction");
-$recipe1->addInstruction("This is my second instruction");
+//$recipe1->addInstruction("This is my first instruction");
+//$recipe1->addInstruction("This is my second instruction");
 
-$recipe1->addTag("Breakfast");
-$recipe1->addTag("Main Course");
+//$recipe1->addTag("Breakfast");
+//$recipe1->addTag("Main Course");
 
-$recipe1->setYield("6 servings");
+//$recipe1->setYield("6 servings");
 
 
 //echo $recipe1->displayRecipe();
 //echo $recipe2->displayRecipe();
-echo $recipe1;
+//echo $recipe1;
 //echo Render::displayRecipe($recipe1);
-echo new Render();
+//echo new Render();
 
-
+echo Render::displayRecipe($belgian_waffles);
 //var_dump($recipe1);
